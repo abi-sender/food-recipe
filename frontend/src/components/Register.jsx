9c31053c39266eeb3b4d7b336cddda6311d0e7c3
abirami <81287215+abi-sender@users.noreply.github.com>
2026-07-14 05:42:27 -0500

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://food-recipe-1-845a.onrender.com/api/auth/register",
         form
       );
 
